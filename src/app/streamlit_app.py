5074ca7c797e38e3759798950d05a859b9183435
fossryan <foss.ryan@gmail.com>
2026-02-08 09:15:58 -0500

    Add files via upload

--- src/app/streamlit_app.py
+++ src/app/streamlit_app.py
@@ -1077,7 +1077,7 @@ def main():
             logo_loaded = False
             for logo_path in logo_paths:
                 try:
-                    st.image(logo_path, width=135)
+                    st.image(logo_path, width=100)
                     logo_loaded = True
                     break
                 except:
